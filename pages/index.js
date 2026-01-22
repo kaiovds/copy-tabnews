10e251317b9341b6c95d688077bafadcbe542ca7
Kaio Dutra <kaiovdutrasouza@gmail.com>
2026-01-21 21:29:54 -0300

    adiciona `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Eu vou me tornar um programador, isso é uma promessa</h1>
+function Home() {
+  return <h1>Eu vou me tornar um programador, isso é uma promessa</h1>;
+}
+
+function Teste() {
+  return <h1>Teste</h1>;
 }
 
 export default Home;
