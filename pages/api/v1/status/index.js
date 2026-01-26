4adfdeb65fcb5b56ec6e30193d7604927465c425
Kaio Dutra <kaiovdutrasouza@gmail.com>
2026-01-25 21:43:04 -0300

    Finaliza o endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
+const dbName = process.env.POSTGRES_DB;
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as resultado");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(dbMaxConnections.rows[0].max_connections),
+        opened_connections: parseInt(dbOpenedConnections.rows[0].count),
+      },
+    },
+  });
 }
 
 export default status;
