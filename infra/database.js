ff40aeaa3380cf75de8c940cedc64482568ed682
Kaio Vinicius Dutra Souza <kaiovdutrasouza@gmail.com>
2026-03-18 02:52:27 +0000

    handles ssl database connection

--- infra/database.js
+++ infra/database.js
@@ -1,46 +1,35 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  let client;
-  try {
-    client = await getNewClient();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-async function getNewClient() {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  ssl: process.env.NODE_ENV === "development" ? false : true,
+});
 
+console.log("Credenciais do Postgres:", {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+try {
   await client.connect();
-  return client;
+  const result = await client.query(queryObject);
+  return result;
+} catch (error) {
+  console.error(error);
+  throw error;
+} finally {
+  await client.end();
+}
 }
 
 export default {
-  query,
-  getNewClient,
+query: query,
 };
-
-function getSSLValues() {
-  if (process.env.POSTGRES_CA) {
-    return {
-      ca: process.env.POSTGRES_CA,
-    };
-  }
-
-  console.log("NODE_ENV: " + process.env.NODE_ENV);
-
-  return process.env.NODE_ENV === "production" ? true : false;
-}
