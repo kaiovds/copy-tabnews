36615c73887497aaa9ea82040912c441eb96cafa
Kaio Vinicius Dutra Souza <kaiovdutrasouza@gmail.com>
2026-03-23 03:15:31 +0000

    adds `api/v1/migrations` endpoint e atualiza arquivos para bater com as aulas

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,41 +1,36 @@
-import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
-import database from "infra/database.js";
+import migrationRunner from 'node-pg-migrate'
+import { join } from 'node:path'
+import database from 'infra/database.js'
 
-export default async function migrations(request, response) {
+export default async function migrations(request, response){
   const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
     dbClient: dbClient,
     dryRun: true,
-    dir: join("infra", "migrations"),
+    dir: join("infra","migrations"),
     direction: "up",
-    verbose: true,
+    verbose: "true",
     migrationsTable: "pgmigrations",
-    //noLock: true,
-  };
-
-  if (request.method === "GET") {
-    console.log("Entrou no GET");
-
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+  }
+  if (request.method === 'GET'){
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions)
     await dbClient.end();
-    response.status(200).json(pendingMigrations);
+  return response.status(200).json(pendingMigrations);
   }
 
-  if (request.method === "POST") {
-    console.log("Entrou no POST");
+  if (request.method === 'POST'){
     const migratedMigrations = await migrationRunner({
       ...defaultMigrationOptions,
       dryRun: false,
-    });
+  });
+  
+  await dbClient.end();
 
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-    response.status(200).json(migratedMigrations);
+  if(migratedMigrations.length > 0){
+    return response.status(201).json(migratedMigrations)
+  }
+  return response.status(200).json(migratedMigrations);
   }
 
-  response.status(405);
+  return response.status(405).end();
 }
